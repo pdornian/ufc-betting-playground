b4ea8460df7a9f9af0d7b28ff7dff49447eb1fea
Aidan Waite <aidanaidan@gmail.com>
2024-03-21 23:48:33 -0400

    fix type issues with parseFloat and add weight height reach policies

--- script.js
+++ script.js
@@ -4,19 +4,24 @@ function parseBoutStrings() {
     // Format the raw input strings into something useful
     for(const boutString of boutStrings) {
         const i = boutString.split(",");
-        output.push({
+        const val = {
             nameA:i[0],
-            heightA:i[56],
-            reachA:i[57],
-            weightA:i[58],
-            oddsA:i[2],
+            heightA:parseFloat(i[56]),
+            reachA:parseFloat(i[57]),
+            weightA:parseFloat(i[58]),
+            oddsA:parseFloat(i[2]),
             nameB:i[1],
-            heightB:i[33],
-            reachB:i[34],
-            weightB:i[35],
-            oddsB:i[3],
+            heightB:parseFloat(i[33]),
+            reachB:parseFloat(i[34]),
+            weightB:parseFloat(i[35]),
+            oddsB:parseFloat(i[3]),
             winner:i[8] === "Red" ? i[0] : i[1],
-        })
+        };
+        if (isNaN(val.oddsA) || isNaN(val.oddsB) || isNaN(val.heightA) || isNaN(val.heightB) || isNaN(val.reachA) || isNaN(val.reachB) || isNaN(val.weightA) || isNaN(val.weightB)) {
+            console.log("Ignore malformed bout:" + boutString);
+        } else {
+            output.push(val);
+        }
     }
     return output;
 }
@@ -24,14 +29,15 @@ function parseBoutStrings() {
 function showTestResults(bouts) {
     const test = document.getElementById("results");
     console.log(test);
-    const startIndex = Math.floor(Math.random() * boutStrings.length);
-    for (var x = startIndex; x < Math.min(startIndex + 5, boutStrings.length); x++) {
+    const startIndex = 0;//Math.floor(Math.random() * boutStrings.length);
+    for (var x = startIndex; x < boutStrings.length; x++) {
         const p = document.createElement("h3");
         const v = bouts[x];
         var text = "";
         text += "name:" + v.nameA + " weight:" + v.weightA + " height:" + v.heightA + " reach:" + v.reachA + " odds:" + v.oddsA;
         text += " VERSUS "
         text += "name:" + v.nameB + " weight:" + v.weightB + " height:" + v.heightB + " reach:" + v.reachB + " odds:" + v.oddsB;
+        text += " WINNER:" + v.winner;
         p.textContent = text;
         test.appendChild(p);
     }
@@ -85,6 +91,74 @@ function betFavorite(bout) {
     return Math.min(bout.oddsA, bout.oddsB);
 }
 
+function betTaller(bout) {
+    if (bout.heightA > bout.heightB) {
+        return betA(bout);
+    } else {
+        return betB(bout);
+    }
+}
+
+function betLongerReach(bout) {
+    if (bout.reachA > bout.reachB) {
+        return betA(bout);
+    } else {
+        return betB(bout);
+    }
+}
+
+function betHeavier(bout) {
+    if (bout.weightA > bout.weightB) {
+        return betA(bout);
+    } else {
+        return betB(bout);
+    }
+}
+
+function betLighter(bout) {
+    if (bout.weightA > bout.weightB) {
+        return betB(bout);
+    } else {
+        return betA(bout);
+    }
+}
+
+function betA(bout) {
+    // Win case
+    if (bout.winner == bout.nameA) {
+        if (bout.oddsA < bout.oddsB) {
+            return 100;
+        } else {
+            return Math.max(bout.oddsA, bout.oddsB);
+        }
+    }
+
+    // Lose case
+    if (bout.oddsA < bout.oddsB) {
+        return bout.oddsA;
+    } else {
+        return -100;
+    }
+}
+
+function betB(bout) {
+    // Win case
+    if (bout.winner == bout.nameB) {
+        if (bout.oddsB < bout.oddsA) {
+            return 100;
+        } else {
+            return Math.max(bout.oddsA, bout.oddsB);
+        }
+    }
+
+    // Lose case
+    if (bout.oddsB < bout.oddsA) {
+        return bout.oddsB;
+    } else {
+        return -100;
+    }
+}
+
 function show(text) {
     const results = document.getElementById("results");
     const p = document.createElement("h3");
@@ -97,7 +171,7 @@ function main() {
 
     // showTestResults(bouts);
 
-    var balance = 0;
+    var balance = 0.0;
     for (const bout of bouts) {
         balance += betUnderdog(bout);
     }
@@ -108,6 +182,30 @@ function main() {
         balance += betFavorite(bout);
     }
     show("Favorite betting policy: " + balance);
+
+    balance = 0;
+    for (const bout of bouts) {
+        balance += betTaller(bout);
+    }
+    show("Taller betting policy: " + balance);
+
+    balance = 0;
+    for (const bout of bouts) {
+        balance += betHeavier(bout);
+    }
+    show("Heavier betting policy: " + balance);
+
+    balance = 0;
+    for (const bout of bouts) {
+        balance += betLighter(bout);
+    }
+    show("Lighter betting policy: " + balance);
+
+    balance = 0;
+    for (const bout of bouts) {
+        balance += betLongerReach(bout);
+    }
+    show("Longer reach betting policy: " + balance);
 }
 
 document.addEventListener('DOMContentLoaded', function() {

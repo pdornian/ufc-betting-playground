ff59e1acaaa9d228a69ccbb59f13cd4281888964
Aidan Waite <aidanaidan@gmail.com>
2024-03-21 21:10:38 -0400

    parse the data in a cool and nice and neat way

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
-alert(boutStrings[852]);
+document.addEventListener('DOMContentLoaded', function() {
+    const bouts = [];
 
-// format boutStrings into something more usable
+    // Format the raw input strings into something useful
+    for(const boutString of boutStrings) {
+        const b = boutString.split(",");
+        bouts.push({
+            nameA:b[0],
+            nameB:b[1],
+            oddsA:b[2],
+            oddsB:b[3],
+            winner:b[8] === "Red" ? b[0] : b[1],
+        })
+    }
 
-/**
- * 0 R_fighter,
- * 1 B_fighter,
- * 2 R_odds,
- * 3 B_odds,
- * 4 R_ev,
- * 5 B_ev,
- * 6 date,
- * 7 location,
- * 8 country,
- * 9 Winner,
- */
+    // For now just output some rows to confirm the data was correctly parsed
+    const test = document.getElementById("test-results");
+    console.log(test);
+    const startIndex = Math.floor(Math.random() * boutStrings.length);
+    for (var x = startIndex; x < Math.min(startIndex + 50, boutStrings.length); x++) {
+        const p = document.createElement("h3");
+        const v = bouts[x];
+        p.textContent = "" + v.nameA + " " + v.oddsA + " vs " + v.nameB + " " + v.oddsB + " winner:" + v.winner;
+        test.appendChild(p);
+    }
 
-// WIP
-// const bouts = [];
-
-// for(const boutString of boutStrings) {
-//     const b = boutString.split(",");
-//     bouts.push({
-//         nameA:b[0],
-//         nameB:b[1],
-//         oddsA:b[3],
-//         oddsB:b[4],
-//         winner:b[9],
-//     })
-// }
+}, false);
